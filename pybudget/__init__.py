b6842c5e5c68c6ac1c9559ed742ad1647a1814c8
Steve Roehl <stevenroehl@gmail.com>
2019-12-20 15:39:48 -0600

    Lots of changes to actually make the UI and started working on allowing uploads

--- pybudget/__init__.py
+++ pybudget/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__,  static_url_path='')
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024
+ALLOWED_EXTENSIONS = {'csv'}
 
 from pybudget import routes
--- pybudget/routes.py
+++ pybudget/routes.py
@@ -1,13 +1,45 @@
-from pybudget import app
-from flask import render_template
+from pybudget import app, ALLOWED_EXTENSIONS
+from flask import render_template, request, redirect, url_for, flash
 from pybudget.helpers import get_summaries
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 @app.route('/budget')
 def budget():
     summaries = get_summaries('1219')
     return render_template('budget.html', summaries=summaries)
+
+
+@app.route('/transactions')
+def transactions():
+    return render_template('transactions.html')
+
+
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        flash('File not in upload')
+        return redirect(url_for('transactions'))
+    file = request.files['file']
+    bank = request.form.get('bank', None)
+    if bank is None:
+        flash('No bank!')
+        return redirect(url_for('transactions'))
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(url_for('transactions'))
+    if file and allowed_file(file.filename):
+        file.sa
+
 
 
 @app.errorhandler(404)
